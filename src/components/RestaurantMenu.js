779394a13dd3469aa23dabddc45b1633d6c5b3e3
PUSARLA TARUN VAMSI <pusarlatarunvamsi@gmail.com>
2024-06-05 16:17:37 +0530

    Add categorise for menu

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import { MENU_API } from "../../utils/constants";
+import { MENU_IMG_ID } from "../../utils/constants";
 const RestaurantMenu = () => {
   const [resInfo, setResInfo] = useState(null);
   const { resId } = useParams();
@@ -14,32 +15,62 @@ const RestaurantMenu = () => {
     const menuData = await fetch(MENU_API + resId);
     const menuJsonResponse = await menuData.json();
 
-    // console.log("menu", menuJsonResponse.data.cards[2].card.card.info.cuisines);
+    console.log("menu", menuJsonResponse);
     setResInfo(menuJsonResponse);
   };
 
   if (resInfo === null) return <Shimmer />;
 
   const resNamePath = resInfo?.data?.cards[2]?.card?.card?.info;
-  const menuPath =
-    resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[3]?.card
-      ?.card?.itemCards;
+  const categories =
+    resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+      (card) => card.card?.card?.itemCards || card.card?.card?.categories
+    );
 
   return (
     <div className="menu">
-      <h1> {resNamePath.name}</h1>
-      <p>
+      <h1 className="resName-menu"> {resNamePath.name}</h1>
+      <p className="cusienes">
         {resNamePath.cuisines.join(",")} - {resNamePath.costForTwoMessage}
       </p>
       <h2>Menu</h2>
-      <ul>
-        {menuPath.map((item) => (
-          <li key={item.card.info.id}>
-            {item.card.info.name} -
-            {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
-          </li>
-        ))}
-      </ul>
+      <div className="menuBox">
+        {categories.map((category, index) => {
+          const categoryTitle = category.card.card.title;
+          const menuItems =
+            category.card.card.itemCards ||
+            category.card.card.categories[0].itemCards;
+
+          return (
+            <div key={index} className="categorySection">
+              <h3 className="categoryTitle">{categoryTitle}</h3>
+              <div className="categoryItems">
+                {menuItems.map((item) => (
+                  <div className="menuCard" key={item.card.info.id}>
+                    <img
+                      className="menu-img"
+                      alt="image"
+                      src={MENU_IMG_ID + item.card.info.imageId}
+                    />
+                    <div> {item.card.info.name}</div>
+                    <div>
+                      Rs{" "}
+                      {item.card.info.price / 100 ||
+                        item.card.info.defaultPrice / 100}{" "}
+                      - ⭐{item.card.info.ratings.aggregatedRating.rating} (
+                      {item.card.info.ratings.aggregatedRating.ratingCount})
+                    </div>
+                    <div className="menudesc">{item.card.info.description}</div>
+                    <div>
+                      <button className="addcart-btn">Add to Cart</button>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- utils/constants.js
+++ utils/constants.js
@@ -10,3 +10,6 @@ export const cordinates = {
 
 export const MENU_API =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId=";
+
+export const MENU_IMG_ID =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
